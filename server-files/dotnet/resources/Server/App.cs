c018adb1099fe4912e9ed2309d31c29e9cfb08f6
Lotsmon <anatolii.sokolov.04@mail.ru>
2021-11-01 04:33:33 +1000

    12

---
+++ server-files/dotnet/resources/Server/App.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using GTANetworkAPI;
+
+namespace Server
+{
+    public class App : Script
+    {
+        [ServerEvent(Event.ResourceStart)]
+        public void ResourceStarEventHandler()
+        {
+            NAPI.Util.ConsoleOutput($"Starting SERVER Resource");
+            NAPI.Util.ConsoleOutput($"DRDRDRDRDR");
+        }
+
+        [ServerEvent(Event.PlayerConnected)]
+        public void Joined()
+        {
+            NAPI.Util.ConsoleOutput($"Connected");
+        }
+
+        [Command("veh")]
+        public void CreateVehicleCommand(Player player, VehicleHash vehicleHash, int color1, int color2) 
+        {
+            NAPI.Vehicle.CreateVehicle(vehicleHash, player.Position, player.Rotation, color1, color2);                                                                                        
+        }
+    }
+}
